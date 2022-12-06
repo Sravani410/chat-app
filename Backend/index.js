cce7fd13ba5c36ce9aa5cec08d70fb00d90bc027
sravani <pylasravani@externlabs.com>
2022-12-06 09:19:22 +0530

    connect to mongodb database

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const cors=require("cors")
+const mongoose=require("mongoose")
+
+const app=express()
+const dotenv=require("dotenv")
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL,{
+     useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=>{
+    console.log("mongodb is connected")
+});
+
+const server=app.listen(process.env.PORT,()=>{
+    console.log(`lisenting to the ${process.env.PORT}`)
+})
