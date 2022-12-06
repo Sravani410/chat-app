b0781eaf666b9fbdd063c134746195fcd7a19650
sravani <pylasravani@externlabs.com>
2022-12-06 23:16:13 +0530

    make the register form with css

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import Logo from "../assets/logo.svg";
 
 function Register() {
+    const [values,setValues]=useState({
+       username:"",
+       email:"",
+       password:"",
+       confirmPassword:"",
+    })
+
     const handleSubmit=(e)=>{
         e.preventDefault();
         alert("form is done")
     }
     const handleChange=(e)=>{
-        // e.preventDefault();
-        alert("working")
+       const { name,value }=e.target
+        setValues({
+          ...values,
+          [name]:value
+        })
     }
+    console.log("values :",values)
   return (
         <>
           <FormContainer>
@@ -36,23 +47,78 @@ function Register() {
   )
 }
 const FormContainer=styled.div `
+    
    height:100vh;
    width:100vw;
    display:flex;
    flex-direction:column;
    justify-content:center;
    gap:1rem;
-   align-item:center;
+   align-items:center;
    background-color:grey;
-   .brand{
-     display:flex;
-     align-items:center;
-     gap:1rem;
-     justify-content:center;
-     img{
-        height:50px;
-     }
-   }
+    .brand {
+        /* border: 1px solid white; */
+        display: flex;
+        align-items: center;
+        gap: .01rem;
+        justify-content: center;
+        img {
+            height: 3.5rem;
+        }
+        h1 {
+            margin: auto;
+            color: white;
+            text-transform: uppercase;
+        }
+    }
+
+    form {
+        /* border: 1px solid white; */
+        height: 40vw;
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+        background-color: #55535376;
+        border-radius: 2rem;
+        padding: 2.2rem 4rem;
+        input {
+            background-color: transparent;    
+            padding: 1rem;
+            border: 0.1rem solid #4e0eff;
+            border-radius: 0.4rem;
+            color: white;
+            width: 100%;
+            font-size: 1rem;
+            &:focus {
+                border: 0.1rem solid #997af0;
+                outline: none;
+            }
+        }
+        button {
+            background-color: #997af0;
+            color: white;
+            padding: 1rem 2rem;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 0.4rem;
+            font-size: 1rem;
+            text-transform: uppercase;
+            transition: 0.5s ease-in-out;
+            &:hover {
+               background-color: #4e0eff;
+            }
+        }
+        span {
+            color: white;
+            text-transform: uppercase;
+            a {
+                color: #4e0eff;
+                text-decoration: none;
+                font-weight: bold;
+            }
+        }
+    }
 `
 export default Register
 
