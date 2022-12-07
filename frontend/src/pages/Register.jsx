ce7a5eb396904db7eaee939a1359e4e3ec60a7f1
sravani <pylasravani@externlabs.com>
2022-12-07 12:44:44 +0530

    add validation functionality in form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import Logo from "../assets/logo.svg";
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
     const [values,setValues]=useState({
@@ -11,9 +13,48 @@ function Register() {
        confirmPassword:"",
     })
 
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        alert("form is done")
+    
+    const toastOption={
+        position:"bottom-right",
+        autoClose:8000,
+        pauseOnHover:true,
+        draggable:true,
+        theme:"dark"
+    }
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        handleValidation()
+        alert("form")
+    }
+    const handleValidation=(e)=>{
+        // console.log(e)
+        // alert("as")
+        const { username,email,password,confirmPassword }=values;
+
+        console.log(username,email,password,confirmPassword)
+
+        if(password !== confirmPassword){
+            console.log("password:",password,"confirmPassword:",confirmPassword)
+            // console.log(e)
+            // console.log("toast message :",toast)
+            toast.error("password and confirm password should be same !",toastOption);
+          return false;
+        }
+        else if(username.length<3){
+            // console.log("username:",username.length)
+          toast.error("username should not be less than 3 !", toastOption);
+          return false
+        }
+         else if(password.length<8){
+            // console.log("username:",username.length)
+          toast.error("password should be atleast minimum 8 characters!", toastOption);
+          return false
+        }
+        else if(email===""){
+            toast.error("email should not be empty",toastOption);
+            return false
+        }
+        return true;
     }
     const handleChange=(e)=>{
        const { name,value }=e.target
@@ -22,32 +63,30 @@ function Register() {
           [name]:value
         })
     }
-    console.log("values :",values)
+    
+
   return (
         <>
           <FormContainer>
-               <form onSubmit={(e)=>handleSubmit(e)}>
-              <div className='brand'>
-                <img src={Logo} alt="Logo" />
-                <h1>Chat app</h1>
-              </div>
-              <input  type="text" placeholder='Username' name="username" onChange={(e)=>handleChange(e)}/>
-              <input type="email" placeholder='Email' name="email" onChange={(e)=>handleChange(e)} />
-              <input type="password" placeholder='password' name="password" onChange={(e)=>handleChange(e)} />
-              <input type="password" placeholder="Re-Enter Password" name="Re-Enter password" onChange={(e)=>handleChange(e)} />
-              <button type="button">Create User</button>
-              <span>have a already account ? <Link to="/login">Login</Link></span>
-           </form>
+            <form onSubmit={handleSubmit}>
+                <div className='brand'>
+                    <img src={Logo} alt="Logo" />
+                    <h1>Chat app</h1>
+                </div>
+                    <input  type="text" placeholder='Username' name="username"  className="input" onChange={handleChange}/>
+                    <input type="email" placeholder='Email' name="email" className='input' onChange={handleChange} />
+                    <input type="password" placeholder='password' name="password" className='input' onChange={handleChange} />
+                    <input type="password" placeholder="confirm Password" name="confirmPassword" className='input' onChange={handleChange} />
+                    <button type="submit">Create User</button>
+                    {/* <input type="submit" value="Submit" /> */}
+                    <span>have a already account ? <Link to="/login">Login</Link></span>
+            </form>
+            <ToastContainer></ToastContainer>
            </FormContainer>
         </>
-          
-          
-       
-   
-  )
+     )
 }
 const FormContainer=styled.div `
-    
    height:100vh;
    width:100vw;
    display:flex;
@@ -71,7 +110,6 @@ const FormContainer=styled.div `
             text-transform: uppercase;
         }
     }
-
     form {
         /* border: 1px solid white; */
         height: 40vw;
