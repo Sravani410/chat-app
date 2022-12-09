b2d7dab5c32a69e0615a433cf4437e9d4ccc3149
sravani <pylasravani@externlabs.com>
2022-12-09 18:52:14 +0530

    chat input done

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import axios from "axios";
 
 import { sendMessageRoute,getMessageRoute } from "../utils/APIRouter";
+import {Logout} from "./Logout"
+import {ChatInput} from "./ChatInput"
 
 import { useState,useEffect,useRef } from "react";
 
@@ -16,6 +18,7 @@ export const ChatContainer =({currentUser,currentChat,socket})=>{
     const scrollRef=useRef()
 
     console.log("message:",message)
+
     useEffect(()=>{
        findMessage();
     },[currentChat]);
@@ -43,11 +46,28 @@ const handleSendMessage=async(msg)=>{
         message:msg,
     })
 
-    const mgs=[...message];
-    
-    
-
+    const msgs=[...message];
+    msgs.push({fromSelf:true,message:msg});
+    setMessage(msgs);
 }
+useEffect(()=>{
+    if(socket.current){
+        socket.current.on("msg-recieve",(msg)=>{
+            setArrivalMessage({fromSelf:false,message:msg})
+        })
+    }
+},[])
+useEffect(()=>{
+    arrivalMessage && setMessage((prev)=>
+    [...prev,arrivalMessage]
+    )
+},[arrivalMessage])
+
+useEffect(()=>{
+    scrollRef.current?.scrollIntoView({behaviour:"smooth"})
+},[message])
+
+console.log("messageatlast:",message)
 
     return (
         <>
@@ -65,11 +85,16 @@ const handleSendMessage=async(msg)=>{
             </div>
             <div className="chat-message">
                 {message.map((el)=>{
+                    console.log("element:",el)
                     return (
                         <div ref={scrollRef} key={uuidv4()}>
-                           {/* <div className={`message ${element.fromSelf ? :"sended" :"recieved"}`}>
-
-                           </div> */}
+                           <div className={`message ${el.fromSelf ? "sended" :"recieved"}`}>
+                             <div className="content">
+                                <p>
+                                    {el.message}
+                                </p>
+                             </div>
+                           </div>
                         </div>
                     )
                 })}
@@ -79,3 +104,92 @@ const handleSendMessage=async(msg)=>{
         </>
     )
 }
+
+const Container = styled.div`
+    padding-top: 1rem;
+
+    display: grid;
+    grid-template-rows: 10% 78% 12%;
+    gap: 0.1rem;
+    @media screen and (min-width : 720px) and (max-width : 1080px) {
+        grid-template-rows: 15% 70% 15%;
+    }
+    @media screen and (min-width : 160px) and (max-width : 720px) {
+        /* border: 1px solid yellow ; */
+        grid-template-rows: 20% 60% 20%;
+    }
+    overflow: hidden;
+    .chat-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        
+        padding: 0 2rem;
+        .user-details {
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+            
+            .avatar {
+                img {
+                    height: 3rem;
+                }
+            }
+            .username {
+                h3 {
+                    color: white;
+                }
+            }
+        }
+    }
+    .chat-messages {
+        /* border: 1px solid white; */
+        padding: 1rem 2rem;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        overflow: auto;
+        &::-webkit-scrollbar {
+            width: 0.2rem;
+            &-thumb {
+                background-color: #ffffff39;
+                width: 0.1rem;
+                border-radius: 1rem;
+            }
+        }
+        .message {
+            /* border: 1px solid yellow; */
+            display: flex;
+            align-items: center;
+            .content {
+                /* border: 1px solid red; */
+                max-width: 40%;
+                overflow-wrap: break-word;
+                padding: 1rem;
+                font-size: 1.1rem;
+                border-radius: 1rem;
+                color: #d1d1d1;
+
+
+
+                @media screen and (min-width: 720px) and (max-width: 1080px) {
+                    max-width: 70%;
+                }
+            }
+        }
+        .sended {
+            justify-content: flex-end;
+            .content {
+                background-color: #4f04ff21;
+            }
+        }
+        .recieved {
+    
+            justify-content: flex-start;
+            .content {
+                background-color: #9900ff20;
+            }
+    
+        }
+    }
+`
