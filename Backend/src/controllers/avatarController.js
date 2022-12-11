025dc42fa1e2d80fb17a99ea6b9e0bbbf2627b1f
sravani <pylasravani@externlabs.com>
2022-12-11 22:48:05 +0530

    updated

--- Backend/src/controllers/avatarController.js
+++ Backend/src/controllers/avatarController.js
@@ -3,8 +3,11 @@ const bcrypt = require("bcrypt");
 
 module.exports.setAvatar = async (req, res, next) => {
   try {
+    //console.log("userId:", req.body);
     const userId = req.params.id;
+    //console.log("userId:",userId)
     const imageAvatar = req.body.image;
+    //console.log("imageAvatar:",imageAvatar)
     const userData = await User.findByIdAndUpdate(
       userId,
       {
@@ -12,6 +15,7 @@ module.exports.setAvatar = async (req, res, next) => {
         avatarImage: imageAvatar,
       },
       { new: true }
+      //console.log("user:",userData)
     );
     return res.status(202).json({
       isSet: userData.isAvatarImageSet,
